70e10e0d64e4a3ad2226d3b64f4350f8609fac1f
Abcunbekannt <61092585+Abcunbekannt@users.noreply.github.com>
2020-05-06 22:12:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,84 +1,46 @@
-import asyncio
-import random
-import os
+from discord.ext import commands
 import discord
-from discord import Member, Guild
-client = discord.Client()
-#########################################################################
-antworten =['Ja', 'Nein', 'Vielleicht', 'Wahrscheinlich', 'Sieht so aus', 'Sehr wahrscheinlich',
-            'Sehr unwahrscheinlich']
-@client.event
+from datetime import datetime
+import asyncio
+import os
+
+
+
+
+
+#@bot.event
+#async def on_ready():
+    #channel = bot.get_channel(707219132701081680) # Deine Channel ID wo die Statistik gesendet werden soll
+    #embed = discord.Embed(title='Server-Stats', color=discord.Color.green(), timestamp=datetime.utcnow())
+    #embed.add_field(name='Mitglieder', value='?')
+    #embed.add_field(name='Online', value='?')
+    #embed.set_footer(text='Zuletzt aktualisiert:')
+    #await channel.send(embed=embed)  # Wenn die Statistik gesendet wurde dieses Event löschen!
+
+
+
+@bot.event
 async def on_ready():
-    print('Wir sind eingeloggt als User {}'.format(client.user.name))
-    client.loop.create_task(status_task())
-async def status_task():
-    colors = [discord.Colour.red(), discord.Colour.orange(), discord.Colour.gold(), discord.Colour.green(),
-              discord.Colour.blue(), discord.Colour.purple()]
+    bot.loop.create_task(stats_task())
+
+
+
+async def stats_task():
     while True:
-        await client.change_presence(activity=discord.Game('Ersteller Canned Heat'), status=discord.Status.online)
-        await asyncio.sleep(5)
-        await client.change_presence(activity=discord.Game('Mein cooler Bot!'), status=discord.Status.online)
-        await asyncio.sleep(5)
-        if client.get_guild(640618905030754338):
-            guild: Guild = client.get_guild(640618905030754338)
-            role = guild.get_role(677575400548859904)
-            if role:
-                if role.position < guild.get_member(client.user.id).top_role.position:
-                    await role.edit(colour=random.choice(colors))
-def is_not_pinned(mess):
-    return not mess.pinned
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    if 't!help' in message.content:
-        await message.channel.send('**Hilfe zum PyBot**\r\n'
-                                   't!help - Zeigt diese Hilfe an\r\n'
-                                   't!userinfo + Name des gewünschten Users - Zeigt Infos zu dem User an\r\n'
-                                   't!8ball + Frage - Der Bot antwortet zufällig auf deine Frage\r\n'
-                                   't!clear + Anzahl der Nachrichten, die gelöscht werden sollen - Löscht die angegebene Anzahl von Nachrichten\r\n'
-                                  
-                                  
-                                  )
-    if message.content.startswith('t!userinfo'):
-        args = message.content.split(' ')
-        if len(args) == 2:
-            member: Member = discord.utils.find(lambda m: args[1] in m.name, message.guild.members)
-            if member:
-                embed = discord.Embed(title='Userinfo für {}'.format(member.name),
-                                      description='Dies ist eine Userinfo für den User {}'.format(member.mention),
-                                      color=0x22a7f0)
-                embed.add_field(name='Server beigetreten', value=member.joined_at.strftime('%d/%m/%Y, %H:%M:%S'),
-                                inline=True)
-                embed.add_field(name='Discord beigetreten', value=member.created_at.strftime('%d/%m/%Y, %H:%M:%S'),
-                                inline=True)
-                rollen = ''
-                for role in member.roles:
-                    if not role.is_default():
-                        rollen += '{} \r\n'.format(role.mention)
-                if rollen:
-                    embed.add_field(name='Rollen', value=rollen, inline=True)
-                embed.set_thumbnail(url=member.avatar_url)
-                embed.set_footer(text='Ich bin ein EmbedFooter.')
-                mess = await message.channel.send(embed=embed)
-                await mess.add_reaction('<img draggable="false" role="img" class="emoji" alt="🚍" src="https://s.w.org/images/core/emoji/12.0.0-1/svg/1f68d.svg">')
-                await mess.add_reaction('a:tut_herz:662606955520458754')
-    if message.content.startswith('t!clear'):
-        if message.author.permissions_in(message.channel).manage_messages:
-            args = message.content.split(' ')
-            if len(args) == 2:
-                if args[1].isdigit():
-                    count = int(args[1]) + 1
-                    deleted = await message.channel.purge(limit=count, check=is_not_pinned)
-                    await message.channel.send('{} Nachrichten gelöscht.'.format(len(deleted) - 1))
-    if message.content.startswith('t!8ball'):
-        args = message.content.split(' ')
-        if len(args) >= 2:
-            frage = ' '.join(args[1:])
-            mess = await message.channel.send('Ich versuche deine Frage `{0}` zu beantworten.'.format(frage))
-            await asyncio.sleep(2)
-            await mess.edit(content='Ich kontaktiere das Orakel...')
-            await asyncio.sleep(2)
-            await mess.edit(content='Deine Antwort zur Frage `{0}` lautet: `{1}`'
-                            .format(frage, random.choice(antworten)))
+        channel = bot.get_channel(707219132701081680)  # Deine Channel ID wo die 1. Message von der Statistik gesendet wurde
+        message = await channel.fetch_message(707224166629113956)  # Die Message ID von der Gesendeten Statistik
+
+        embed = discord.Embed(title='Server-Stats', color=discord.Color.green(), timestamp=datetime.utcnow(), description='Hier hast du eine kleine Übersicht über uns.')
+        embed.add_field(name='Mitglieder', value=channel.guild.member_count, inline=True)
+        embed.add_field(name='Online', value=len({m.id for m in channel.guild.members if m.status is not discord.Status.offline}), inline=True)
+        #embed.add_field(name=' ⠀⠀',value= ' ⠀⠀', inline=False)
+        #embed.add_field(name='Team Mitglieder:', value='Noch nicht festgelegt', inline=True)
+        #embed.add_field(name='VIP Mitglieder:', value='Noch nicht festgelegt', inline=True)
+        #embed.set_footer(text='Zuletzt aktualisiert:')
+        await message.edit(embed=embed)
+        await asyncio.sleep(600)  # = 10 Minuten
+
+
+
+#bot.run(TOKEN)
 client.run(os.getenv('Token'))
